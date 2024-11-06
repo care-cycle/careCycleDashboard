0bc78b76ed0f783d7eca500c5a03e51a82cc6264
Evan Roubekas <evan@roubekas.com>
2024-11-06 14:03:00 -0700

    .

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 const DEV_DOMAIN = 'http://10.0.0.155:5173';
-const PROD_DOMAIN = 'app.nodable.ai';
+const PROD_DOMAIN = 'clerk.nodable.ai';
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Clerk Publishable Key');
